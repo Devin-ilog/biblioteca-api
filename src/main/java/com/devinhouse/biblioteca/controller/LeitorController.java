1f1212e5f18d3e97b7a4d6f8e9bdb38c86837894
tiagoamp <tiagoamp@gmail.com>
2023-06-20 20:44:29 -0300

    Implementa insercao de Leitor

--- src/main/java/com/devinhouse/biblioteca/controller/LeitorController.java
+++ src/main/java/com/devinhouse/biblioteca/controller/LeitorController.java
@@ -1,17 +1,17 @@
 package com.devinhouse.biblioteca.controller;
 
+import com.devinhouse.biblioteca.dto.LeitorRequest;
 import com.devinhouse.biblioteca.dto.LeitorResponse;
 import com.devinhouse.biblioteca.model.Leitor;
 import com.devinhouse.biblioteca.service.LeitorService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -36,14 +36,14 @@ public class LeitorController {
     }
 
 
-//    @PostMapping
-//    public ResponseEntity<LeitorResponse> inserir(@RequestBody @Valid LeitorRequest request) {
-//        Leitor leitor = mapper.map(request, Leitor.class);
-//        leitor = leitorService.incluir(leitor);
-//        LeitorResponse resp = mapper.map(leitor, LeitorResponse.class);
-//        return ResponseEntity.created(URI.create(leitor.getCpf().toString())).body(resp);
-//    }
-//
+    @PostMapping
+    public ResponseEntity<LeitorResponse> inserir(@RequestBody @Valid LeitorRequest request) {
+        Leitor leitor = mapper.map(request, Leitor.class);
+        leitor = leitorService.incluir(leitor);
+        LeitorResponse resp = mapper.map(leitor, LeitorResponse.class);
+        return ResponseEntity.created(URI.create(leitor.getCpf().toString())).body(resp);
+    }
+
 //    @DeleteMapping("{cpf}")
 //    public ResponseEntity excluir(@PathVariable Long cpf) {
 //        leitorService.excluir(cpf);
