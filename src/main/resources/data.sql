1fedfce31870a6738d03662d9a25da6d6d94abeb
tiagoamp <tiagoamp@gmail.com>
2023-06-19 22:10:32 -0300

    Fix dados SQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO LIVROS(ISBN, TITULO, AUTORES, DISPONIVEL) VALUES ('978-0548952610', 'api Princípios Matemáticos de Filosofia Natural', 'Isaac Newton', 1);
-INSERT INTO LIVROS(ISBN, TITULO, AUTORES, DISPONIVEL) VALUES ('978-8599296578', 'api O Guia do Mochileiro das Galáxias', 'Douglas Adams', 1);
-INSERT INTO LIVROS(ISBN, TITULO, AUTORES, DISPONIVEL) VALUES ('978-8576082675', 'api Clean Code', 'Robert C. Martin', 1);
+INSERT INTO LIVROS(ISBN, TITULO, AUTORES, DISPONIVEL) VALUES ('978-0548952610', 'Princípios Matemáticos de Filosofia Natural', 'Isaac Newton', 1);
+INSERT INTO LIVROS(ISBN, TITULO, AUTORES, DISPONIVEL) VALUES ('978-8599296578', 'O Guia do Mochileiro das Galáxias', 'Douglas Adams', 1);
+INSERT INTO LIVROS(ISBN, TITULO, AUTORES, DISPONIVEL) VALUES ('978-8576082675', 'Clean Code', 'Robert C. Martin', 1);
